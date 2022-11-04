278a0ab43b34bf68a795835c1546582da14d054e
Joe <spizjo01@gmail.com>
2022-11-04 12:53:48 -0600

    ditto

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative "boot"
 
-require "rails"
+require "rails/all"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
@@ -12,6 +12,8 @@ require "action_mailbox/engine"
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'sprockets/railtie'
+
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
