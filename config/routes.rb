6c89ab5edb9d5e073752d2fe720b762cab06ed42
Joe <spizjo01@gmail.com>
2022-11-14 10:00:54 -0700

    setting up front to back end connection for render

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   resources :recipefoods
   resources :recipes do
     resources :recipefoods
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   get '/me', to: "users#show"
+
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
